39db024acc0f574beb8fcd5725bc9b8dfc00d65a
GauravJ3 <gaurav@192.168.1.5>
2024-03-01 15:10:15 +0530

    feat: first commit expense-tracker

---
+++ go.mod
@@ -0,0 +1,3 @@
+module expense_tracker.com
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,41 @@
+// Simple Expense Tracker CLI Application
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Expense struct {
+	Category string
+	Value    float64
+	Date     time.Time
+}
+
+// define an array of expenses
+var expenses []Expense
+
+func addExpense(category string, value float64, date time.Time) {
+	expense := Expense{Category: category, Value: value, Date: date}
+	expenses = append(expenses, expense)
+
+	fmt.Print("Expense added !! ", expenses)
+}
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter expense Type, Amount and date of expense to track.")
+	input, _ := reader.ReadString('\n')
+
+	parts := strings.Split(strings.TrimSpace(input), ",")
+	category := parts[0]
+	value, _ := strconv.ParseFloat(parts[1], 64)
+	date, _ := time.Parse("2006-01-02", parts[2])
+
+	addExpense(category, value, date)
+}
